87e1a4f422a59aafd8b12195da8da16b396d15a5
linzhanguca <lzhang12@uca.edu>
2024-11-22 18:30:35 -0600

    add camera res config and update camera test

--- scripts/unit_test/camera.py
+++ scripts/unit_test/camera.py
@@ -1,19 +1,31 @@
 """
-If ssh from other machine, please enable X11 forwarding, either `ssh -X` or `ssh -Y`
+If logged in remotely, please enable X11 forwarding, either `ssh -X` or `ssh -Y`
 """
 import sys
+import os
+import json
 import cv2
 from picamera2 import Picamera2
 from time import sleep
 
-# SETUP
 print("Please adjust lens focus if blurry")
+# SETUP
+# Load configs
+params_file_path = os.path.join(os.path.dirname(sys.path[0]), 'configs.json')
+with open(params_file_path, 'r') as file:
+    params = json.load(file)
+# params_file = open(params_file_path)
+# params = json.load(params_file)
+# Constants
+WIDTH = params['image_width']
+HEIGHT = params['image_height']
+# Config Pi Camera
 for i in reversed(range(1, 4)):
     print(i)
     sleep(1)
 cv2.startWindowThread()
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": 'RGB888', "size": (640, 480)}))
+picam2.configure(picam2.create_preview_configuration(main={"format": 'RGB888', "size": (WIDTH, HEIGHT)}))
 picam2.start()
 
 # LOOP
