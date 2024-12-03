32343de99232384051411d9bb3731fb88a73cb1e
Lin Zhang <linzhank@gmail.com>
2024-12-02 19:13:26 -0600

    update dataset
    encode contents to pytorch tensors

--- scripts/train.py
+++ scripts/train.py
@@ -1,25 +1,33 @@
 import os
 import sys
-import numpy as np
+# import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
+import torchvision.models as models
 from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision.transforms import v2
+from torchvision.io import read_image
 import matplotlib.pyplot as plt
-import convnets
-import cv2 as cv
+# import convnets
+# import cv2 as cv
 
 # Pass in command line arguments for data diretory name
-# e.g. python train.py 2022-02-22-22-22
+# e.g. python train.py 2023-12-13-14-15
 if len(sys.argv) != 2:
-    print('Training script needs data!!!')
+    print('Training script needs data! Please provide date and time!')
     sys.exit(1)  # exit with an error code
 else:
     data_datetime = sys.argv[1]
 
 # Designate processing unit for CNN training
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
 print(f"Using {DEVICE} device")
 
 
@@ -30,21 +38,37 @@ class BearCartDataset(Dataset):
     def __init__(self, annotations_file, img_dir):
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
-        self.transform = v2.ToTensor()
+        self.trans_in = v2.Compose([v2.ToDtype(torch.float32, scale=True)])
+        self.trans_out = v2.Compose([v2.ToDtype(torch.float32, scale=False)])
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = cv.imread(img_path, cv.IMREAD_COLOR)
-        image_tensor = self.transform(image)
-        steering = self.img_labels.iloc[idx, 1].astype(np.float32)
-        throttle = self.img_labels.iloc[idx, 2].astype(np.float32)
-        return image_tensor.float(), steering, throttle
+        image = self.trans_in(read_image(img_path))
+        steering = torch.as_tensor(self.img_labels.iloc[idx, 1], dtype=torch.float32)
+        throttle = torch.as_tensor(self.img_labels.iloc[idx, 2], dtype=torch.float32)
+        return image, steering, throttle
 
+# class EfficientBearNet(nn.Module):
+#     def __init__(self, num_outputs=2):
+#         super(EfficientBearNet, self).__init__()
+#         self.efficientnet = models.efficientnet_b3(weights=None)
+#         in_features = self.efficientnet.classifier[1].in_features
+#         self.efficientnet.classifier = nn.Sequential(
+#             nn.Dropout(p=0.1, inplace=True),
+#             nn.Linear(in_features, num_outputs)
+#         )
+#
+#     def forward(self, x):
+#         return self.efficientnet(x)
 
 def train(dataloader, model, loss_fn, optimizer):
+    """
+    training function for 1 epoch
+    """
+
     model.train()
     num_used_samples = 0
     ep_loss = 0.
@@ -75,56 +99,56 @@ def test(dataloader, model, loss_fn):
     return ep_loss
 
 
-# MAIN
-# Create a dataset
-data_dir = os.path.join(os.path.dirname(sys.path[0]), 'data', data_datetime)
-annotations_file = os.path.join(data_dir, 'labels.csv')  # the name of the csv file
-img_dir = os.path.join(data_dir, 'images') # the name of the folder with all the images in it
-bearcart_dataset = BearCartDataset(annotations_file, img_dir)
-print(f"data length: {len(bearcart_dataset)}")
-
-# Create training dataloader and test dataloader
-train_size = round(len(bearcart_dataset)*0.9)
-test_size = len(bearcart_dataset) - train_size
-print(f"train size: {train_size}, test size: {test_size}")
-train_data, test_data = random_split(bearcart_dataset, [train_size, test_size])
-train_dataloader = DataLoader(train_data, batch_size=125)
-test_dataloader = DataLoader(test_data, batch_size=125)
-
-# Create model - Pass in image size
-model = convnets.DonkeyNet().to(DEVICE)  # choose the architecture class from cnn_network.py
-# Hyper-parameters (lr=0.001, epochs=10 | lr=0.0001, epochs=15 or 20)
-lr = 0.001
-optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.0001)
-# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.8)
-loss_fn = nn.MSELoss()
-epochs = 15 # switch back to 15 epochs
-# Optimize the model
-train_losses = []
-test_losses = []
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    ep_train_loss = train(train_dataloader, model, loss_fn, optimizer)
-    ep_test_loss = test(test_dataloader, model, loss_fn)
-    print(f"epoch {t+1} training loss: {ep_train_loss}, testing loss: {ep_test_loss}")
-    current_lr = optimizer.param_groups[0]['lr']
-    print(f"Learning rate after scheduler step: {current_lr}")
-    # save values
-    train_losses.append(ep_train_loss)
-    test_losses.append(ep_test_loss)
-    # Apply the learning rate scheduler after each epoch
-    # scheduler.step()
-
-print("Optimize Done!")
-
-# Graph training process
-pilot_title = f'{model._get_name()}-{epochs}epochs-{lr}lr'
-plt.plot(range(epochs), train_losses, 'b--', label='Training')
-plt.plot(range(epochs), test_losses, 'orange', label='Test')
-plt.xlabel('Epoch')
-plt.ylabel('MSE Loss')
-plt.legend()
-plt.title(pilot_title)
-plt.savefig(os.path.join(data_dir, f'{pilot_title}.png'))
-# Save the model
-torch.save(model.state_dict(), os.path.join(data_dir, f'{pilot_title}.pth'))
+# # MAIN
+# # Create a dataset
+# data_dir = os.path.join(os.path.dirname(sys.path[0]), 'data', data_datetime)
+# annotations_file = os.path.join(data_dir, 'labels.csv')  # the name of the csv file
+# img_dir = os.path.join(data_dir, 'images') # the name of the folder with all the images in it
+# bearcart_dataset = BearCartDataset(annotations_file, img_dir)
+# print(f"data length: {len(bearcart_dataset)}")
+#
+# # Create training dataloader and test dataloader
+# train_size = round(len(bearcart_dataset)*0.915)
+# test_size = len(bearcart_dataset) - train_size
+# print(f"train size: {train_size}, test size: {test_size}")
+# train_data, test_data = random_split(bearcart_dataset, [train_size, test_size])
+# train_dataloader = DataLoader(train_data, batch_size=256, shuffle=True)
+# test_dataloader = DataLoader(test_data, batch_size=256)
+#
+# # Create model - Pass in image size
+# model = EfficientBearNet().to(DEVICE)  # choose the architecture class from cnn_network.py
+# # Hyper-parameters (lr=0.001, epochs=10 | lr=0.0001, epochs=15 or 20)
+# lr = 0.001
+# optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.0001)
+# # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.8)
+# loss_fn = nn.MSELoss()
+# epochs = 15 # switch back to 15 epochs
+# # Optimize the model
+# train_losses = []
+# test_losses = []
+#     print(f"Epoch {t+1}\n-------------------------------")
+# for t in range(epochs):
+#     ep_train_loss = train(train_dataloader, model, loss_fn, optimizer)
+#     ep_test_loss = test(test_dataloader, model, loss_fn)
+#     print(f"epoch {t+1} training loss: {ep_train_loss}, testing loss: {ep_test_loss}")
+#     current_lr = optimizer.param_groups[0]['lr']
+#     print(f"Learning rate after scheduler step: {current_lr}")
+#     # save values
+#     train_losses.append(ep_train_loss)
+#     test_losses.append(ep_test_loss)
+#     # Apply the learning rate scheduler after each epoch
+#     # scheduler.step()
+#
+# print("Optimize Done!")
+#
+# # Graph training process
+# pilot_title = f'{model._get_name()}-{epochs}epochs-{lr}lr'
+# plt.plot(range(epochs), train_losses, 'b--', label='Training')
+# plt.plot(range(epochs), test_losses, 'orange', label='Test')
+# plt.xlabel('Epoch')
+# plt.ylabel('MSE Loss')
+# plt.legend()
+# plt.title(pilot_title)
+# plt.savefig(os.path.join(data_dir, f'{pilot_title}.png'))
+# # Save the model
+# torch.save(model.state_dict(), os.path.join(data_dir, f'{pilot_title}.pth'))
