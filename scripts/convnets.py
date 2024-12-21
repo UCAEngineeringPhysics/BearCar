a9d742e7aaabac3dec728043f928f8fc9438c0e7
linzhanguca <lzhang12@uca.edu>
2024-12-20 22:58:51 -0600

    update model structure
    inspired by ResNet

--- scripts/convnets.py
+++ scripts/convnets.py
@@ -35,33 +35,36 @@ class BearCartNet(nn.Module):
 
     def __init__(self):
         super(BearCartNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=8, stride=2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=5, stride=2)
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=5)
-        self.conv5 = nn.Conv2d(128, 128, kernel_size=3)
-        self.conv6 = nn.Conv2d(128, 128, kernel_size=3)
-        self.conv7 = nn.Conv2d(128, 128, kernel_size=3)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=2)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3)
 
-        self.fc1 = nn.Linear(128*15*15, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 2)
         self.relu = nn.ReLU()
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.avg_pool = nn.AvgPool2d(kernel_size=1)
+
+        self.fc1 = nn.Linear(256*7*7, 1000)
+        self.fc2 = nn.Linear(1000, 2)
 
     def forward(self, x):  # 224
-        x = self.relu(self.conv1(x))  # (224 - 8) / 2 + 1 = 109
-        x = self.relu(self.conv2(x))  # (109 - 5) / 2 + 1 = 53
-        x = self.relu(self.conv3(x))  # (53 - 5) / 2 + 1 = 25
-        x = self.relu(self.conv4(x))  # (25 - 5) + 1 = 21
-        x = self.relu(self.conv5(x))  # (21 - 3) + 1 = 19
-        x = self.relu(self.conv6(x))  # (19 - 3) + 1 = 17
-        x = self.relu(self.conv7(x))  # (17 - 3) + 1 = 15
+        x = self.relu(self.conv1(x))  # (224 - 7 + 2 * 3) / 2 + 1 = 112.5
+        x = self.max_pool(x)  # (112 - 3 + 2 * 1) / 2 + 1 = 56.5
+        x = self.relu(self.conv2(x))  # (56 - 3) + 1 = 54
+        x = self.relu(self.conv3(x))  # (54 - 3) / 2 + 1 = 26.5
+        x = self.relu(self.conv4(x))  # (26 - 3) + 1 = 24
+        x = self.relu(self.conv5(x))  # (24 - 3) / 2 + 1 = 11.5
+        x = self.relu(self.conv6(x))  # (11 - 3) + 1 = 9
+        x = self.relu(self.conv7(x))  # (9 - 3) + 1 = 7
+        x = self.avg_pool(x)  # (7 - 1) + 1 = 7
 
         x = x.view(x.size(0), -1)  # flatten
 
         x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        y = self.fc3(x)
+        y = self.fc2(x)
         return y
 
 
