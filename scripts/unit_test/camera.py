566c1b077c704ac803d18b93763e5f7dcf10e0b9
linzhanguca <lzhang12@uca.edu>
2024-12-21 21:52:00 -0600

    update camera test with frame rate config

--- scripts/unit_test/camera.py
+++ scripts/unit_test/camera.py
@@ -9,24 +9,32 @@ from time import time
 
 print("Please adjust lens focus if image is blurry")
 # SETUP
+# Load configs
+params_file_path = os.path.join(os.path.dirname(sys.path[0]), 'configs.json')
+with open(params_file_path, 'r') as file:
+    params = json.load(file)
 # Config Pi Camera
 cv2.startWindowThread()
 picam = Picamera2()
 picam.configure(
     picam.create_preview_configuration(
         main={"format": 'RGB888', "size": (224, 224)},
-        controls={"FrameDurationLimits": (41667, 41667)},  # 24 FPS
+        controls={
+            "FrameDurationLimits": (
+                int(1000_000 / params['frame_rate']), int(1000_000 / params['frame_rate'])
+            )
+        },  # 24 FPS
     )
 )
 # Start Pi Camera with a count down
 picam.start()
-for i in reversed(range(72)):
+for i in reversed(range(3 * params['frame_rate'])):
     frame = picam.capture_array()
     if frame is None:
         print("No frame received. TERMINATE!")
         sys.exit()
-    if not i % 24:
-        print(i/24)  # count down 3, 2, 1 sec
+    if not i % params['frame_rate']:
+        print(i/params['frame_rate'])  # count down 3, 2, 1 sec
 # Init timer for FPS computing
 frame_counts = 0
 frame_rate = 0.
