6e63fddae148f50d2fc8e71a9c168d43ce3cdc7b
linzhanguca <lzhang12@uca.edu>
2025-05-27 12:14:43 -0500

    update env setup

--- setup_pi_env.sh
+++ setup_pi_env.sh
@@ -1,5 +1,7 @@
 cd $HOME
 sudo apt update && sudo apt upgrade -y
 curl -LsSf https://astral.sh/uv/install.sh | sh
-echo 'eval "$(uv generate-shell-completion bash)"' >>~/.bashrc
+cd $HOME/BearCart/ && uv venv
+echo 'eval "$(uv generate-shell-completion bash)"' >>$HOME/.bashrc
+echo 'alias gobc="source $HOME/BearCart/.venv/bin/activate"' >>$HOME/.bashrc
 source $HOME/.bashrc
