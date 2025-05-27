586a4b32a561ce8c9458aaf3babeee9cefe9c730
linzhanguca <lzhang12@uca.edu>
2025-05-27 10:58:18 -0500

    add camera test dependencies

--- setup_pi_env.sh
+++ setup_pi_env.sh
@@ -1,4 +1,5 @@
 cd $HOME
 sudo apt update && sudo apt upgrade -y
+curl -LsSf https://astral.sh/uv/install.sh | sh
 echo 'eval "$(uv generate-shell-completion bash)"' >>~/.bashrc
 source $HOME/.bashrc
