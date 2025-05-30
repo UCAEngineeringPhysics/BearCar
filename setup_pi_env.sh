f87a02343d2981f9ecb7cf7b1ac4477f69d633b7
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2025-05-30 18:49:12 -0500

    Update setup_pi_env.sh

--- setup_pi_env.sh
+++ setup_pi_env.sh
@@ -1,10 +1,10 @@
 cd $HOME
 sudo apt update && sudo apt upgrade -y
 curl -LsSf https://astral.sh/uv/install.sh | sh
-echo 'export PATH=$PATH:$HOME/.local/bin' >>$HOME/.bashrc
+echo 'export PATH="$HOME/.local/bin:$PATH"' >>$HOME/.bashrc
 echo 'eval "$(uv generate-shell-completion bash)"' >>$HOME/.bashrc
 # cd $HOME/BearCart/ && uv venv
-# echo 'alias gobc="source $HOME/BearCart/.venv/bin/activate"' >>$HOME/.bashrc
+echo 'alias gobear="source $HOME/BearCart/.venv/bin/activate"' >>$HOME/.bashrc
 source $HOME/.bashrc
 cd $HOME/BearCart/ && uv sync
 echo 'You may start BearCart environemnt now'
