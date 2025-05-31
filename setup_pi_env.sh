8c988737a73bad373f75f74d3bcf6ac1644cbeb5
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2025-05-30 22:14:53 -0500

    Update setup_pi_env.sh
    color output message

--- setup_pi_env.sh
+++ setup_pi_env.sh
@@ -2,10 +2,14 @@ cd $HOME
 sudo apt update && sudo apt upgrade -y
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-echo 'eval "$(uv generate-shell-completion bash)"' >>$HOME/.bashrc
-echo 'alias gobear="source $HOME/BearCart/.venv/bin/activate"' >>$HOME/.bashrc
-source $HOME/.bashrc
 cd $HOME/BearCart
 uv venv --system-site-packages
+echo 'alias gobear="source $HOME/BearCart/.venv/bin/activate"' >>$HOME/.bashrc
+echo 'eval "$(uv generate-shell-completion bash)"' >>$HOME/.bashrc
 uv sync
-echo 'You may start BearCart environemnt now'
+source $HOME/.bashrc
+
+# Coloring output
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
+echo -e 'You may start BearCart environemnt now: ${PURPLE}gobear${NC}'
