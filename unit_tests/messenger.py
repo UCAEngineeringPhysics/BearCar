37cce7278fa840e584836292e585435014388ef9
linzhanguca <lzhang12@uca.edu>
2025-06-05 14:10:49 -0500

    update messenger test with varied mode, steering, throttle values

--- unit_tests/messenger.py
+++ unit_tests/messenger.py
@@ -19,17 +19,29 @@ for i in reversed(range(3)):
 # SETUP
 messenger = Serial(port='/dev/ttyACM0', baudrate=115200)
 print(messenger.name)
+# Constants
+modes = ('n', 'r', 'a', 'n')
+dutycycles = (1_350_000, 1_400_000, 1_650_000, 1_600_000)
+# Variables
+st_dc = 1_500_000
+th_dc = 1_500_000
 
 # LOOP
-# msg = "n, 1_876_543, 1_543_210\n".encode('utf-8')
-msg = "Hello from RPi\n".encode('utf-8')
 for i in range(100):
-    messenger.write(f"Hello from RPi: {i}\n".encode('utf-8'))
-    # messenger.write(msg)
-    # print("Transmitting")  # debug
+    # messenger.write(f"Hello from RPi: {i}\n".encode('utf-8'))  # simple test
+    if i < 80:
+        st_dc = dutycycles[int(i/20)]
+        th_dc = dutycycles[int(i/20)]
+        msg = f"{modes[int(i/20)]}, {st_dc}, {th_dc}\n".encode('utf-8')
+    else:
+        msg = f"p, {st_dc}, {th_dc}\n".encode('utf-8')
+    messenger.write(msg)
+    # print("RPi transmitting")  # debug
     sleep(0.1)
     if messenger.inWaiting() > 0:
         reply = messenger.readline()
         reply = reply.decode('utf-8', 'ignore')
         print(f"[Pico Response] {reply}")
+messenger.write(f"p, {1_500_000}, {1_500_000}\n".encode('utf-8'))
+sleep(0.5)
 messenger.close()
