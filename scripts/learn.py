0d9f0222e96ec9e06081de716f38bb436f5e85d8
linzhank <linzhank@gmail.com>
2025-06-09 19:30:50 -0500

    update learning script according to new project structure

--- scripts/learn.py
+++ scripts/learn.py
@@ -8,25 +8,7 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms import v2
 from torchvision.io import decode_image
 import matplotlib.pyplot as plt
-from pilot_models import BearNet
-
-# SETUP
-# Pass in data directory name as CLI argument
-# e.g. python train.py 2025-12-13-14-15
-if len(sys.argv) != 2:
-    print("Training script needs data! Please provide date and time!")
-    sys.exit(1)  # exit with an error code
-else:
-    data_datetime = sys.argv[1]
-# Designate processing unit for CNN training
-DEVICE = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
-print(f"Using {DEVICE} device")
+from pilot_architecture import BearNet
 
 
 # Define custom dataset
@@ -49,6 +31,24 @@ class BearCartDataset(Dataset):
         target_steering = torch.as_tensor(self.labels_df.iloc[idx, 1], dtype=torch.float32)
         target_throttle = torch.as_tensor(self.labels_df.iloc[idx, 2], dtype=torch.float32)
         return image, target_steering, target_throttle
+
+# SETUP
+# Pass in data directory name as CLI argument
+# e.g. python train.py 2025-12-13-14-15
+if len(sys.argv) != 2:
+    print("Training script needs data! Please provide date and time!")
+    sys.exit(1)  # exit with an error code
+else:
+    data_datetime = sys.argv[1]
+# Designate processing unit for CNN training
+DEVICE = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {DEVICE} device")
 
 
 # Define training process
@@ -80,9 +80,9 @@ def validate(dataloader, model, loss_fn):
     model.eval()
     ep_loss = 0.0
     with torch.no_grad():
-        for i, (im, st, th) in enumerate(dataloader):
+        for i, (img, st, th) in enumerate(dataloader):
             target = torch.stack((st, th), dim=-1)
-            feature, target = im.to(DEVICE), target.to(DEVICE)
+            feature, target = img.to(DEVICE), target.to(DEVICE)
             pred = model(feature)
             batch_loss = loss_fn(pred, target)
             ep_loss = (ep_loss * i + batch_loss.item()) / (i + 1)
@@ -90,12 +90,12 @@ def validate(dataloader, model, loss_fn):
 
 # Instantiate dataset
 data_dir = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
     "data",
     data_datetime,
 )
 raw_labels_file_path = os.path.join(data_dir, "labels.csv")  # the name of the csv file
-# Split train/val (9:1)
+# Split train/val (90:10)
 raw_labels_df = pd.read_csv(
     raw_labels_file_path,
     header=None,
@@ -110,31 +110,24 @@ train_df = raw_labels_df.drop(val_ids)
 val_df = val_df.reset_index(drop=True)
 train_df = train_df.reset_index(drop=True)
 print(f"train size: {len(train_df)}, validation size: {len(val_df)}")
+# Generate train/val annotation files
 train_df.to_csv(
-    os.path.join(data_dir, "labels_train.csv"), index=False, header=False
+    os.path.join(data_dir, "labels_train.csv"),
+    index=False,
+    header=False,
 )
 val_df.to_csv(
-    os.path.join(data_dir, "labels_val.csv"), index=False, header=False
+    os.path.join(data_dir, "labels_val.csv"),
+    index=False,
+    header=False,
 )
-# train_label_file_path = os.path.join(data_dir, "labels_train.csv")
-# val_label_file_path = os.path.join(data_dir, "labels_val.csv")
 # Create dataset and dataloader
 img_dir = os.path.join(data_dir, "images")
-# val_set = BearCartDataset(val_label_file_path, img_dir)
 train_set = BearCartDataset(os.path.join(data_dir, "labels_train.csv"), img_dir)
 val_set = BearCartDataset(os.path.join(data_dir, "labels_val.csv"), img_dir)
 train_dataloader = DataLoader(train_set, batch_size=128, shuffle=True)
 val_dataloader = DataLoader(val_set, batch_size=128)
-# Instantiate model and config training
-model = BearNet().to(DEVICE)  # choose the architecture class from cnn_network.py
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-loss_fn = nn.MSELoss()
-epochs = 64
-patience = 7
-best_loss = float("inf")  # best loss on test data
-since_best_counter = 0
-train_losses = []
-val_losses = []
+# Create directory for saving trained models
 model_dir = os.path.join(data_dir, "models")
 if not os.path.exists(model_dir):
     try:
@@ -142,19 +135,29 @@ if not os.path.exists(model_dir):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+# Instantiate model and config training
+model = BearNet().to(DEVICE)  # choose the architecture class from cnn_network.py
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+loss_fn = nn.MSELoss()
+max_epochs = 64
+patience = 7
+best_loss = float("inf")  # best loss on validation data
+since_best_counter = 0
+train_losses = []
+val_losses = []
 
 
 # LOOP
 # Optimize model
-for ep in range(epochs):
+for ep in range(max_epochs):
     print(f"Epoch {ep + 1}\n-------------------------------")
     ep_train_loss = train(train_dataloader, model, loss_fn, optimizer)
     ep_val_loss = validate(val_dataloader, model, loss_fn)
-    print(f"Epoch {ep + 1} training loss: {ep_train_loss}, testing loss: {ep_val_loss}")
+    print(f"Epoch {ep + 1} training loss: {ep_train_loss}, validation loss: {ep_val_loss}")
     train_losses.append(ep_train_loss)
     val_losses.append(ep_val_loss)
     model_name = f"ep{ep + 1}-mse{ep_val_loss:.4f}"
-    torch.save(model.state_dict(), os.path.join(data_dir, f"models/{model_name}.pth"))
+    torch.save(model.state_dict(), os.path.join(model_dir, f"{model_name}.pth"))
     # Early stopping
     if ep_val_loss < best_loss:
         best_loss = ep_val_loss
@@ -188,4 +191,4 @@ plt.xlabel("Epoch")
 plt.ylabel("MSE Loss")
 plt.legend()
 plt.title(model_name)
-plt.savefig(os.path.join(data_dir, f"models/{model_name}.png"))
+plt.savefig(os.path.join(model_dir, "learning_curve.png"))
