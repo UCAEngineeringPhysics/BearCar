8b0e283fd97bf30289d298bec8d2b1f66225a10e
linzhanguca <lzhang12@uca.edu>
2025-06-14 00:38:28 -0500

    update project setup sh

--- setup_pi_env.sh
+++ setup_pi_env.sh
@@ -2,14 +2,14 @@ cd $HOME
 sudo apt update && sudo apt upgrade -y
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-cd $HOME/BearCart
+cd $HOME/BearCar
 uv venv --system-site-packages
 echo 'alias gobear="source $HOME/BearCart/.venv/bin/activate"' >>$HOME/.bashrc
 echo 'eval "$(uv generate-shell-completion bash)"' >>$HOME/.bashrc
-uv sync
+uv sync --group rpi
 source $HOME/.bashrc
 
 # Coloring output
 PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
-echo -e "You may start BearCart environemnt now: ${PURPLE}gobear${NC}"
+echo -e "You may start BearCar environemnt now: ${PURPLE}gobear${NC}"
