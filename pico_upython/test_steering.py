876f0b2a609d964fefa58c6754248950b86da056
linzhank <linzhank@gmail.com>
2025-06-20 16:39:05 -0500

    update servo test script

--- pico_upython/test_steering.py
+++ pico_upython/test_steering.py
@@ -1,4 +1,4 @@
-from machine import Pin, PWM
+from machine import Pin, PWM, reset
 from time import sleep
 
 # SETUP
@@ -15,5 +15,15 @@ for i in reversed(range(1000000, 2000000, 10000)):
     print(i)
     sleep(0.2)
 servo.duty_ns(1500000)
+sleep(1)
+servo.duty_ns(1800000)
+sleep(1)
+servo.duty_ns(1500000)
+sleep(1)
+servo.duty_ns(1200000)
+sleep(1)
+servo.duty_ns(1500000)
+sleep(1)
 servo.deinit()
+reset()
 
