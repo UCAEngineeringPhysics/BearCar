1b67db66689d7ad1dd1721416bdc0a5c2d0328d8
linzhank <linzhank@gmail.com>
2025-06-20 16:56:03 -0500

    update servo test scripts
    1. added more prompts
    2. decreased pause intervals for fast turns

--- pico_upython/test_steering.py
+++ pico_upython/test_steering.py
@@ -4,7 +4,7 @@ from time import sleep
 # SAFETY CHECK
 is_contacted = "y"
 while is_contacted is not "n":
-    print("Please lift BearCar up and remove everything that is making the contact")
+    print("Please lift BearCar up and remove everything that is making the contact\n")
     is_contacted = input("Is anything touching any wheel of BearCar? (Y/n)")
 print("BearCar is about to turn!!!")
 
@@ -13,24 +13,26 @@ servo = PWM(Pin(17))
 servo.freq(50)
 
 # LOOP
+print("<<<--- <<-- <-W\n")
 for i in range(1000000, 2000000, 10000):
     servo.duty_ns(i)
     print(i)
     sleep(0.2)
+print("W -> -->> --->>>\n")
 for i in reversed(range(1000000, 2000000, 10000)):
     servo.duty_ns(i)
     print(i)
     sleep(0.2)
 servo.duty_ns(1500000)
-sleep(1)
+sleep(0.5)
 servo.duty_ns(1800000)
-sleep(1)
+sleep(0.5)
 servo.duty_ns(1500000)
-sleep(1)
+sleep(0.5)
 servo.duty_ns(1200000)
-sleep(1)
+sleep(0.5)
 servo.duty_ns(1500000)
-sleep(1)
+sleep(0.5)
 servo.deinit()
 reset()
 
