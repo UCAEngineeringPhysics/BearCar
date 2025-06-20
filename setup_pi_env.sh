9d4d4cfcb2ec5625c6cadf21a9d188128e135acf
linzhank <linzhank@gmail.com>
2025-06-20 16:27:34 -0500

    fix bearcar venv activate alias

--- setup_pi_env.sh
+++ setup_pi_env.sh
@@ -4,7 +4,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 cd $HOME/BearCar
 uv venv --system-site-packages
-echo 'alias gobear="source $HOME/BearCart/.venv/bin/activate"' >>$HOME/.bashrc
+echo 'alias gobear="source $HOME/BearCar/.venv/bin/activate"' >>$HOME/.bashrc
 echo 'eval "$(uv generate-shell-completion bash)"' >>$HOME/.bashrc
 uv sync --group rpi
 source $HOME/.bashrc
