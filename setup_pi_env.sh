1cf1db92a5d8d4df770f53070e5539e9f68df3a9
linzhanguca <lzhang12@uca.edu>
2026-05-29 00:55:59 -0500

    specify torch versions

--- setup_pi_env.sh
+++ setup_pi_env.sh
@@ -4,12 +4,12 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 cd $HOME/BearCar
 uv venv --system-site-packages --python /usr/bin/python3
-uv sync --group rpi
+uv sync --group rpi --extra cpu
 echo 'alias gobear="source $HOME/BearCar/.venv/bin/activate"' >>$HOME/.bashrc
 echo 'eval "$(uv generate-shell-completion bash)"' >>$HOME/.bashrc
-source $HOME/.bashrc
 
 # Coloring output
 PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
 echo -e "You may start BearCar environemnt now: ${PURPLE}gobear${NC}"
+source $HOME/.bashrc
--- setup_server_env.sh
+++ setup_server_env.sh
@@ -4,7 +4,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 cd $HOME/BearCar
 uv venv --system-site-packages --python /usr/bin/python3
-uv sync --group server
+uv sync --group server --extra cu130
 echo 'alias gobear="source $HOME/BearCar/.venv/bin/activate"' >>$HOME/.bashrc
 echo 'eval "$(uv generate-shell-completion bash)"' >>$HOME/.bashrc
 source $HOME/.bashrc
